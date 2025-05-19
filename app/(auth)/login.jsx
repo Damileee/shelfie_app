8413f6f4cc693b46d88694bf17d50f5887b5a62b
Damileee <olasojudamilare6@gmail.com>
2025-05-19 15:15:18 +0100

    add login functionality with ThemedButton and update styles

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,11 +1,18 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
 
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 
 const Login = () => {
+
+  const handleSubmit = () => {
+    console.log("Login form submitted");
+  }
+
   return (
     <ThemedView style={styles.container}>
       
@@ -13,6 +20,10 @@ const Login = () => {
       <ThemedText title={true} style={styles.title}>
         Login to Your Account
       </ThemedText>
+
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: '#f2f2f2' }}>Login</Text>
+      </ThemedButton>
 
       <Spacer height={100} />
       <Link href="/register" replace>
@@ -31,10 +42,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8,
   },
 })
