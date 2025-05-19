b0eff6e6d293bcf4d630c98446426564d52c2eaf
Damileee <olasojudamilare6@gmail.com>
2025-05-19 15:20:56 +0100

    refactor RootLayout component for improved readability and structure

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,30 +1,25 @@
-import { Stack } from "expo-router";
-import { StyleSheet, Text, useColorScheme, View } from "react-native";
-import { Colors } from "../constants/Colors";
-import { StatusBar } from "expo-status-bar";
+import { Stack } from "expo-router"
+import { Colors } from "../constants/Colors"
+import { useColorScheme } from "react-native"
+import { StatusBar } from "expo-status-bar"
 
-const RootLayout = () => {
-    const colorScheme = useColorScheme();
-    const theme = Colors[colorScheme] ?? Colors.light;
+export default function RootLayout() {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
 
-    return (
-        <> 
-            <StatusBar value= "auto" />
-            <Stack screenOptions={{
-                headerStyle: {
-                    backgroundColor: theme.navBackground,
-                },
-                headerTintColor: theme.title,
-            }}>
-                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-                <Stack.Screen name="index" options={{ title: 'Home' }} />
-            </Stack>
-        </>
-    );
-};
+  return (
+    <>
+      <StatusBar value="auto" />
+      <Stack screenOptions={{
+        headerStyle: { backgroundColor: theme.navBackground },
+        headerTintColor: theme.title,
+      }}>
+        {/* Groups */}
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
 
-export default RootLayout;
-
-const styles = StyleSheet.create({
-
-});
+        {/* Individual Screens */}
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+      </Stack>
+    </>
+  )
+}
